bdc265615094d9c52ac232db4056c198f5c1a553
Rintaro Kawagishi <rintaro.kawagishi@gmail.com>
2020-12-23 18:54:45 +0900

    🔨 we can now centrally manage the schemes data, and the main page simply pulls info from there.

--- main.js
+++ main.js
@@ -4,6 +4,21 @@ const { Scheme, Process, ProcessType } = require('./models.js')
 
 var win
 var schemes = {}
+addRandomSchemes()
+
+function addRandomSchemes() {
+    process1 = new Process(processName='Open google', type=ProcessType.open_url_in_browser, url='https://www.google.com')
+    process2 = new Process(processName='Open apple', type=ProcessType.open_url_in_browser, url='https://www.apple.com')
+    process3 = new Process(processName='Open w3school', type=ProcessType.open_url_in_browser, url='https://www.w3schools.com')
+    process4 = new Process(processName='Open youtube', type=ProcessType.open_url_in_browser, url='https://www.youtube.com')
+    process5 = new Process(processName='Open cam uni', type=ProcessType.open_url_in_browser, url='https://www.cam.ac.uk')
+    scheme1 = new Scheme(schemeName='Scheme 1 bro', id=null, processes=[process1, process2, process3,])
+    scheme2 = new Scheme(schemeName='Yeah man scheme 2', id=null, processes=[process2, process3, process4,])
+    scheme3 = new Scheme(schemeName='wow scheme 3', id=null, processes=[process5, process2, process3,])
+    for (scheme of [scheme1, scheme2, scheme3]) {
+        schemes[scheme.id] = scheme
+    }
+}
 
 function createWindow () {
     win = new BrowserWindow({
@@ -38,15 +53,14 @@ app.on('activate', () => {
     }
 })
 
-// If the user wants to edit a specific scheme, we move to the scheme edit page
-ipcMain.on('edit_scheme', (event, scheme_name) => {
-    process1 = new Process(processName='Open google', type=ProcessType.open_url_in_browser, url='https://www.google.com')
-    process2 = new Process(processName='Open apple', type=ProcessType.open_url_in_browser, url='https://www.apple.com')
-    process3 = new Process(processName='Open w3school', type=ProcessType.open_url_in_browser, url='https://www.w3schools.com')
-    scheme1 = new Scheme(schemeName='Yay scheme name bro', id=null, processes=[process1, process2, process3,])
-    schemes[scheme1.id] = scheme1
+ipcMain.on('requestSchemes', (event) => {
+    console.log('requested schemes')
+    event.reply('requestSchemes-reply', schemes)
+})
 
-    win.loadFile('edit_scheme.html', {query: {"scheme_id": scheme1.id}})
+// If the user wants to edit a specific scheme, we move to the scheme edit page
+ipcMain.on('edit_scheme', (event, schemeID) => {
+    win.loadFile('edit_scheme.html', {query: {"scheme_id": schemeID}})
 })
 
 ipcMain.on('requestSchemeData', (event, scheme_id) => {
