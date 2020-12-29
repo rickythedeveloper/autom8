8dc28ff8bc1f68b887274a4404280246f5a73948
Rintaro Kawagishi <rintaro.kawagishi@gmail.com>
2020-12-30 00:20:33 +0900

    ⚙️ Added @types/uuid to the dev depedency ⚙️ Converted main.js and models.js to Typescript 🏋️ compile command takes care of compiling ts and copying static files

--- build/models.js
+++ build/models.js
@@ -1,110 +1,76 @@
-const { v4: uuidv4 } = require('uuid');
-
-class Scheme {
-    constructor(schemeName, id=null, processes=[]) {
-        this.schemeName = schemeName
-        if (id == null) {
-            this.id = uuidv4()
-        } else {
-            this.id = id
-        }
-
-        this.processes = processes
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Variable = exports.Process = exports.ProcessTypes = exports.Scheme = void 0;
+var Scheme = /** @class */ (function () {
+    function Scheme(schemeName, id, processes) {
+        this.schemeName = schemeName;
+        this.id = id;
+        this.processes = processes;
     }
-
-    runScheme() {
-        for (process of this.processes) {
-            process.runProcess()
+    Scheme.prototype.runScheme = function () {
+        for (var _i = 0, _a = this.processes; _i < _a.length; _i++) {
+            var eachProcess = _a[_i];
+            eachProcess.runProcess();
         }
-    }
-}
-
-const ProcessType = {
+    };
+    return Scheme;
+}());
+exports.Scheme = Scheme;
+var ProcessTypes = {
     openURLInBrowser: {
-        typeName: 'openURLInBrowser',
+        typeName: "openURLInBrowser",
         nInputs: 1,
         nOutputs: 0,
     },
     dummy: {
-        typeName: 'dummy',
-        nInputs: '3',
-        nOutputs: '2',
+        typeName: "dummy",
+        nInputs: 3,
+        nOutputs: 2,
+    },
+    invalid: {
+        typeName: "invalid",
+        nInputs: 0,
+        nOutputs: 0,
+    },
+};
+exports.ProcessTypes = ProcessTypes;
+var Process = /** @class */ (function () {
+    function Process(data) {
+        this.data = data;
     }
-}
-
-class Process {
-    constructor(data) {
-        /**
-         * data must contain
-         * processName: string
-         * processType: one item of ProcessType
-         * id: uuidv4 (if not contained, it will be added during initialisation)
-         * inputVars: [Variable]
-         * outputVars: [Variable]
-         */
-        if (!('processName' in data) || !('processType' in data)) {
-            console.log('Process data does not contain mandatory data when the Process object initialised.')
+    Process.prototype.runProcess = function () {
+        if (this.isInvalidProcess()) {
+            return;
         }
-        this.data = data
-
-        if (!('id' in this.data))  {
-            console.log('id is set now')
-            this.data['id'] = uuidv4()
-        } else {
-            console.log('id was already set?')
+        var processType = this.data.processType;
+        var inputVars = this.data.inputVars;
+        var outputVars = this.data.outputVars;
+        if (!(inputVars.length == processType.nInputs &&
+            outputVars.length == processType.nOutputs)) {
+            console.log("N. of inputs or outputs is incorrect");
         }
-    }
-
-    runProcess() {
-        const processType = this.dataValue('processType')
-        if (processType != null) {
-            const inputVars = this.dataValue('inputVars')
-            if (inputVars.length != processType.nInputs) {
-                console.log('N. of inputs is not right')
-            }
-            const outputVars = this.dataValue('outputVars')
-            if (outputVars.length != processType.nOutputs) {
-                console.log('N. of outputs is not right')
-            } 
-
-            if (inputVars != null) {
-                switch (processType) {
-                    case ProcessType.openURLInBrowser:
-                        if (inputVars.length != 1) {
-                            console.log('openURLInBrowser type only accepts one input')
-                        }
-                        const url = inputVars[0].data.value
-                        if (url != null) {
-                            require('electron').shell.openExternal(url)
-                        }
-                        break
-                    case ProcessType.dummy:
-                        console.log('Running dummy process')
-                        break
-                    default:
-                        console.log('The process type could not be determined')
-                }
-            }
+        switch (processType) {
+            case ProcessTypes.openURLInBrowser:
+                var url = inputVars[0].data.value;
+                require("electron").shell.openExternal(url);
+                break;
+            case ProcessTypes.dummy:
+                console.log("Running dummy process");
+                break;
+            default:
+                console.log("The process type could not be determined");
         }
+    };
+    Process.prototype.isInvalidProcess = function () {
+        return this.data.processType.typeName == "invalid";
+    };
+    return Process;
+}());
+exports.Process = Process;
+var Variable = /** @class */ (function () {
+    function Variable(data) {
+        this.data = data;
     }
-
-    dataValue(key) {
-        if (key in this.data) {
-            return this.data[key]
-        }
-        console.log('The process data does not contain: ' + key)
-        return null
-    }
-}
-
-class Variable {
-    constructor(data) {
-        /**
-         * data must contain:
-         * name, value
-         */
-        this.data = data
-    }
-}
-
-module.exports = { Scheme, Process, ProcessType, Variable}
+    return Variable;
+}());
+exports.Variable = Variable;
--- src/main.ts
+++ src/main.ts
@@ -1,152 +1,167 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // const { app, BrowserWindow, ipcMain, shell } = require("electron");
-var electron_1 = require("electron");
-var uuid_1 = require("uuid");
-var models_1 = require("./models");
-var win;
-var schemes = {};
+import { app, BrowserWindow, ipcMain, shell } from "electron";
+import { v4 as uuidv4 } from "uuid";
+
+import { Scheme, ProcessTypes, Process, Variable } from "./models";
+
+let win: BrowserWindow;
+let schemes: { [id: string]: Scheme } = {};
 addRandomSchemes();
+
 function addRandomSchemes() {
-    var googleURL = new models_1.Variable({
+    const googleURL = new Variable({
         name: "Google URL",
         value: "https://www.google.com",
     });
-    var appleURL = new models_1.Variable({
+    const appleURL = new Variable({
         name: "Apple URL",
         value: "https://www.apple.com",
     });
-    var w3schoolsURL = new models_1.Variable({
+    const w3schoolsURL = new Variable({
         name: "W3Schools URL",
         value: "https://www.w3schools.com",
     });
-    var youtubeURL = new models_1.Variable({
+    const youtubeURL = new Variable({
         name: "Youtube URL",
         value: "https://www.youtube.com",
     });
-    var camUniURL = new models_1.Variable({
+    const camUniURL = new Variable({
         name: "Cambridge University URL",
         value: "https://www.cam.ac.uk",
     });
-    var process1 = new models_1.Process({
+
+    const process1 = new Process({
         processName: "Open Google",
-        processType: models_1.ProcessTypes.openURLInBrowser,
-        id: uuid_1.v4(),
+        processType: ProcessTypes.openURLInBrowser,
+        id: uuidv4(),
         inputVars: [googleURL],
         outputVars: [],
     });
-    var process2 = new models_1.Process({
+    const process2 = new Process({
         processName: "Open apple",
-        processType: models_1.ProcessTypes.openURLInBrowser,
-        id: uuid_1.v4(),
+        processType: ProcessTypes.openURLInBrowser,
+        id: uuidv4(),
         inputVars: [appleURL],
         outputVars: [],
     });
-    var process3 = new models_1.Process({
+    const process3 = new Process({
         processName: "Open w3school",
-        processType: models_1.ProcessTypes.openURLInBrowser,
-        id: uuid_1.v4(),
+        processType: ProcessTypes.openURLInBrowser,
+        id: uuidv4(),
         inputVars: [w3schoolsURL],
         outputVars: [],
     });
-    var process4 = new models_1.Process({
+    const process4 = new Process({
         processName: "Open youtube",
-        processType: models_1.ProcessTypes.openURLInBrowser,
-        id: uuid_1.v4(),
+        processType: ProcessTypes.openURLInBrowser,
+        id: uuidv4(),
         inputVars: [youtubeURL],
         outputVars: [],
     });
-    var process5 = new models_1.Process({
+    const process5 = new Process({
         processName: "Open cam uni",
-        processType: models_1.ProcessTypes.openURLInBrowser,
-        id: uuid_1.v4(),
+        processType: ProcessTypes.openURLInBrowser,
+        id: uuidv4(),
         inputVars: [camUniURL],
         outputVars: [],
     });
-    var process6 = new models_1.Process({
+    const process6 = new Process({
         processName: "Dummy 1",
-        processType: models_1.ProcessTypes.dummy,
-        id: uuid_1.v4(),
+        processType: ProcessTypes.dummy,
+        id: uuidv4(),
         inputVars: [googleURL, appleURL, w3schoolsURL],
         outputVars: [youtubeURL, camUniURL],
     });
-    new models_1.Scheme("heylo", "ay", []);
-    var scheme1 = new models_1.Scheme("Scheme 1 bro", uuid_1.v4(), [
+
+    new Scheme("heylo", "ay", []);
+    const scheme1 = new Scheme("Scheme 1 bro", uuidv4(), [
         process1,
         process2,
         process3,
     ]);
-    var scheme2 = new models_1.Scheme("Yeah man scheme 2", uuid_1.v4(), [
+    const scheme2 = new Scheme("Yeah man scheme 2", uuidv4(), [
         process2,
         process3,
         process4,
         process5,
     ]);
-    var scheme3 = new models_1.Scheme("wow scheme 3", uuid_1.v4(), [
+    const scheme3 = new Scheme("wow scheme 3", uuidv4(), [
         process6,
         process5,
         process2,
         process3,
     ]);
-    for (var _i = 0, _a = [scheme1, scheme2, scheme3]; _i < _a.length; _i++) {
-        var scheme = _a[_i];
+
+    for (const scheme of [scheme1, scheme2, scheme3]) {
         schemes[scheme.id] = scheme;
     }
 }
+
 function createWindow() {
-    win = new electron_1.BrowserWindow({
+    win = new BrowserWindow({
         width: 1400,
         height: 1000,
         webPreferences: {
             nodeIntegration: true,
         },
     });
+
     goToHome();
+
     // This is called when user clicks on <a> link with target="_blank" attribute
     win.webContents.on("new-window", function (event, url) {
         event.preventDefault();
-        electron_1.shell.openExternal(url);
+        shell.openExternal(url);
     });
 }
-electron_1.app.whenReady().then(createWindow);
-electron_1.app.on("window-all-closed", function () {
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
     // if (process.platform !== 'darwin') {
     //     app.quit()
     // }
-    electron_1.app.quit();
+    app.quit();
 });
-electron_1.app.on("activate", function () {
-    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
 });
-electron_1.ipcMain.on("requestSchemes", function (event) {
+
+ipcMain.on("requestSchemes", (event) => {
     console.log("requested schemes");
     event.reply("requestSchemes-reply", schemes);
 });
+
 // If the user wants to edit a specific scheme, we move to the scheme edit page
-electron_1.ipcMain.on("editScheme", function (event, schemeID) {
+ipcMain.on("editScheme", (event, schemeID) => {
     win.loadFile("build/edit_scheme.html", { query: { schemeID: schemeID } });
 });
-electron_1.ipcMain.on("requestSchemeData", function (event, schemeID) {
+
+ipcMain.on("requestSchemeData", (event, schemeID) => {
     if (schemeID in schemes) {
         event.reply("requestSchemeData-reply", schemes[schemeID]);
-    }
-    else {
+    } else {
         console.log("We could not find the scheme requested.");
     }
 });
-electron_1.ipcMain.on("openURLInBrowser", function (event, url) {
+
+ipcMain.on("openURLInBrowser", (event, url) => {
     console.log("Trying to open " + url);
     require("electron").shell.openExternal(url);
 });
-electron_1.ipcMain.on("runScheme", function (event, schemeID) {
-    var scheme = schemes[schemeID];
+
+ipcMain.on("runScheme", (event, schemeID) => {
+    const scheme = schemes[schemeID];
     scheme.runScheme();
 });
-electron_1.ipcMain.on("goToHome", function (event) {
+
+ipcMain.on("goToHome", (event) => {
     goToHome();
 });
+
 function goToHome() {
     win.loadFile("build/index.html");
 }
