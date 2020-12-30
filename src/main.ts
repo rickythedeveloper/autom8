d90f55c50cdc36e3586798f692d18ffa0e288bdf
Rintaro Kawagishi <rintaro.kawagishi@gmail.com>
2020-12-30 22:55:29 +0900

    ⚙️ formating

--- src/main.ts
+++ src/main.ts
@@ -9,164 +9,150 @@ let schemes: { [id: string]: Scheme } = {};
 addRandomSchemes();
 
 function addRandomSchemes() {
-    const googleURL = new Variable({
-        name: "Google URL",
-        value: "https://www.google.com",
-        id: uuidv4()
-    });
-    const appleURL = new Variable({
-        name: "Apple URL",
-        value: "https://www.apple.com",
-        id: uuidv4()
-    });
-    const w3schoolsURL = new Variable({
-        name: "W3Schools URL",
-        value: "https://www.w3schools.com",
-        id: uuidv4()
-    });
-    const youtubeURL = new Variable({
-        name: "Youtube URL",
-        value: "https://www.youtube.com",
-        id: uuidv4()
-    });
-    const camUniURL = new Variable({
-        name: "Cambridge University URL",
-        value: "https://www.cam.ac.uk",
-        id: uuidv4()
-    });
+	const googleURL = new Variable({
+		name: "Google URL",
+		value: "https://www.google.com",
+		id: uuidv4(),
+	});
+	const appleURL = new Variable({
+		name: "Apple URL",
+		value: "https://www.apple.com",
+		id: uuidv4(),
+	});
+	const w3schoolsURL = new Variable({
+		name: "W3Schools URL",
+		value: "https://www.w3schools.com",
+		id: uuidv4(),
+	});
+	const youtubeURL = new Variable({
+		name: "Youtube URL",
+		value: "https://www.youtube.com",
+		id: uuidv4(),
+	});
+	const camUniURL = new Variable({
+		name: "Cambridge University URL",
+		value: "https://www.cam.ac.uk",
+		id: uuidv4(),
+	});
 
-    const process1 = new Process({
-        processName: "Open Google",
-        processType: ProcessTypes.openURLInBrowser,
-        id: uuidv4(),
-        inputVars: [googleURL],
-        outputVars: [],
-    });
-    const process2 = new Process({
-        processName: "Open apple",
-        processType: ProcessTypes.openURLInBrowser,
-        id: uuidv4(),
-        inputVars: [appleURL],
-        outputVars: [],
-    });
-    const process3 = new Process({
-        processName: "Open w3school",
-        processType: ProcessTypes.openURLInBrowser,
-        id: uuidv4(),
-        inputVars: [w3schoolsURL],
-        outputVars: [],
-    });
-    const process4 = new Process({
-        processName: "Open youtube",
-        processType: ProcessTypes.openURLInBrowser,
-        id: uuidv4(),
-        inputVars: [youtubeURL],
-        outputVars: [],
-    });
-    const process5 = new Process({
-        processName: "Open cam uni",
-        processType: ProcessTypes.openURLInBrowser,
-        id: uuidv4(),
-        inputVars: [camUniURL],
-        outputVars: [],
-    });
-    const process6 = new Process({
-        processName: "Dummy 1",
-        processType: ProcessTypes.dummy,
-        id: uuidv4(),
-        inputVars: [googleURL, appleURL, w3schoolsURL],
-        outputVars: [youtubeURL, camUniURL],
-    });
+	const process1 = new Process({
+		processName: "Open Google",
+		processType: ProcessTypes.openURLInBrowser,
+		id: uuidv4(),
+		inputVars: [googleURL],
+		outputVars: [],
+	});
+	const process2 = new Process({
+		processName: "Open apple",
+		processType: ProcessTypes.openURLInBrowser,
+		id: uuidv4(),
+		inputVars: [appleURL],
+		outputVars: [],
+	});
+	const process3 = new Process({
+		processName: "Open w3school",
+		processType: ProcessTypes.openURLInBrowser,
+		id: uuidv4(),
+		inputVars: [w3schoolsURL],
+		outputVars: [],
+	});
+	const process4 = new Process({
+		processName: "Open youtube",
+		processType: ProcessTypes.openURLInBrowser,
+		id: uuidv4(),
+		inputVars: [youtubeURL],
+		outputVars: [],
+	});
+	const process5 = new Process({
+		processName: "Open cam uni",
+		processType: ProcessTypes.openURLInBrowser,
+		id: uuidv4(),
+		inputVars: [camUniURL],
+		outputVars: [],
+	});
+	const process6 = new Process({
+		processName: "Dummy 1",
+		processType: ProcessTypes.dummy,
+		id: uuidv4(),
+		inputVars: [googleURL, appleURL, w3schoolsURL],
+		outputVars: [youtubeURL, camUniURL],
+	});
 
-    new Scheme("heylo", "ay", []);
-    const scheme1 = new Scheme("Scheme 1 bro", uuidv4(), [
-        process1,
-        process2,
-        process3,
-    ]);
-    const scheme2 = new Scheme("Yeah man scheme 2", uuidv4(), [
-        process2,
-        process3,
-        process4,
-        process5,
-    ]);
-    const scheme3 = new Scheme("wow scheme 3", uuidv4(), [
-        process6,
-        process5,
-        process2,
-        process3,
-    ]);
+	new Scheme("heylo", "ay", []);
+	const scheme1 = new Scheme("Scheme 1 bro", uuidv4(), [process1, process2, process3]);
+	const scheme2 = new Scheme("Yeah man scheme 2", uuidv4(), [process2, process3, process4, process5]);
+	const scheme3 = new Scheme("wow scheme 3", uuidv4(), [process6, process5, process2, process3]);
 
-    for (const scheme of [scheme1, scheme2, scheme3]) {
-        schemes[scheme.id] = scheme;
-    }
+	for (const scheme of [scheme1, scheme2, scheme3]) {
+		schemes[scheme.id] = scheme;
+	}
 }
 
 function createWindow() {
-    win = new BrowserWindow({
-        width: 1400,
-        height: 1000,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
+	win = new BrowserWindow({
+		width: 1400,
+		height: 1000,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	});
 
-    goToHome();
+	goToHome();
 
-    // This is called when user clicks on <a> link with target="_blank" attribute
-    win.webContents.on("new-window", function (event, url) {
-        event.preventDefault();
-        shell.openExternal(url);
-    });
+	// This is called when user clicks on <a> link with target="_blank" attribute
+	win.webContents.on("new-window", function (event, url) {
+		event.preventDefault();
+		shell.openExternal(url);
+	});
 }
 
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
-    // if (process.platform !== 'darwin') {
-    //     app.quit()
-    // }
-    app.quit();
+	// if (process.platform !== 'darwin') {
+	//     app.quit()
+	// }
+	app.quit();
 });
 
 app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
 
 ipcMain.on("requestSchemes", (event) => {
-    console.log("requested schemes");
-    event.reply("requestSchemes-reply", schemes);
+	console.log("requested schemes");
+	event.reply("requestSchemes-reply", schemes);
 });
 
 // If the user wants to edit a specific scheme, we move to the scheme edit page
 ipcMain.on("editScheme", (event, schemeID) => {
-    win.loadFile("build/edit_scheme.html", { query: { schemeID: schemeID } });
+	win.loadFile("build/edit_scheme.html", { query: { schemeID: schemeID } });
 });
 
 ipcMain.on("requestSchemeData", (event, schemeID) => {
-    if (schemeID in schemes) {
-        event.reply("requestSchemeData-reply", schemes[schemeID]);
-    } else {
-        console.log("We could not find the scheme requested.");
-    }
+	if (schemeID in schemes) {
+		event.reply("requestSchemeData-reply", schemes[schemeID]);
+	} else {
+		console.log("We could not find the scheme requested.");
+	}
 });
 
 ipcMain.on("openURLInBrowser", (event, url) => {
-    console.log("Trying to open " + url);
-    require("electron").shell.openExternal(url);
+	console.log("Trying to open " + url);
+	require("electron").shell.openExternal(url);
 });
 
 ipcMain.on("runScheme", (event, schemeID) => {
-    const scheme = schemes[schemeID];
-    scheme.runScheme();
+	const scheme = schemes[schemeID];
+	scheme.runScheme();
 });
 
 ipcMain.on("goToHome", (event) => {
-    goToHome();
+	goToHome();
 });
 
 function goToHome() {
-    win.loadFile("build/index.html");
+	win.loadFile("build/index.html");
 }
--- src/models.ts
+++ src/models.ts
@@ -1,115 +1,102 @@
 class Scheme {
-    schemeName: string;
-    id: string;
-    processes: Process[];
+	schemeName: string;
+	id: string;
+	processes: Process[];
 
-    constructor(schemeName: string, id: string, processes: Process[]) {
-        this.schemeName = schemeName;
-        this.id = id;
-        this.processes = processes;
-    }
+	constructor(schemeName: string, id: string, processes: Process[]) {
+		this.schemeName = schemeName;
+		this.id = id;
+		this.processes = processes;
+	}
 
-    runScheme() {
-        for (let eachProcess of this.processes) {
-            eachProcess.runProcess();
-        }
-    }
+	runScheme() {
+		for (let eachProcess of this.processes) {
+			eachProcess.runProcess();
+		}
+	}
 }
 
 interface ProcessType {
-    typeName: string;
-    nInputs: number;
-    nOutputs: number;
+	typeName: string;
+	nInputs: number;
+	nOutputs: number;
 }
 
 const ProcessTypes = {
-    openURLInBrowser: {
-        typeName: "openURLInBrowser",
-        nInputs: 1,
-        nOutputs: 0,
-    },
-    dummy: {
-        typeName: "dummy",
-        nInputs: 3,
-        nOutputs: 2,
-    },
-    invalid: {
-        typeName: "invalid",
-        nInputs: 0,
-        nOutputs: 0,
-    },
+	openURLInBrowser: {
+		typeName: "openURLInBrowser",
+		nInputs: 1,
+		nOutputs: 0,
+	},
+	dummy: {
+		typeName: "dummy",
+		nInputs: 3,
+		nOutputs: 2,
+	},
+	invalid: {
+		typeName: "invalid",
+		nInputs: 0,
+		nOutputs: 0,
+	},
 };
 
 interface ProcessData {
-    processName: string;
-    processType: ProcessType;
-    id: string;
-    inputVars: Variable[];
-    outputVars: Variable[];
+	processName: string;
+	processType: ProcessType;
+	id: string;
+	inputVars: Variable[];
+	outputVars: Variable[];
 }
 
 class Process {
-    data: ProcessData;
+	data: ProcessData;
 
-    constructor(data: ProcessData) {
-        this.data = data;
-    }
+	constructor(data: ProcessData) {
+		this.data = data;
+	}
 
-    runProcess() {
-        if (this.isInvalidProcess()) {
-            return;
-        }
-        const processType = this.data.processType;
-        const inputVars = this.data.inputVars;
-        const outputVars = this.data.outputVars;
-        if (
-            !(
-                inputVars.length == processType.nInputs &&
-                outputVars.length == processType.nOutputs
-            )
-        ) {
-            console.log("N. of inputs or outputs is incorrect");
-        }
+	runProcess() {
+		if (this.isInvalidProcess()) {
+			return;
+		}
+		const processType = this.data.processType;
+		const inputVars = this.data.inputVars;
+		const outputVars = this.data.outputVars;
+		if (!(inputVars.length == processType.nInputs && outputVars.length == processType.nOutputs)) {
+			console.log("N. of inputs or outputs is incorrect");
+		}
 
-        switch (processType) {
-            case ProcessTypes.openURLInBrowser:
-                const url: string = inputVars[0].data.value;
-                require("electron").shell.openExternal(url);
-                break;
-            case ProcessTypes.dummy:
-                console.log("Running dummy process");
-                break;
-            default:
-                console.log("The process type could not be determined");
-        }
-    }
+		switch (processType) {
+			case ProcessTypes.openURLInBrowser:
+				const url: string = inputVars[0].data.value;
+				require("electron").shell.openExternal(url);
+				break;
+			case ProcessTypes.dummy:
+				console.log("Running dummy process");
+				break;
+			default:
+				console.log("The process type could not be determined");
+		}
+	}
 
-    isInvalidProcess() {
-        return this.data.processType.typeName == "invalid";
-    }
+	isInvalidProcess() {
+		return this.data.processType.typeName == "invalid";
+	}
 }
 
 interface VariableData {
-    name: string;
-    value: any;
-    id: string;
+	name: string;
+	value: any;
+	id: string;
 }
 
 class Variable {
-    data: VariableData;
+	data: VariableData;
 
-    constructor(data: VariableData) {
-        this.data = data;
-    }
+	constructor(data: VariableData) {
+		this.data = data;
+	}
 }
 
 // module.exports = { Scheme, ProcessTypes, Process, Variable };
-export {
-    Scheme,
-    ProcessType,
-    ProcessTypes,
-    ProcessData,
-    Process,
-    VariableData,
-    Variable,
-};
+export { Scheme, ProcessType, ProcessTypes, ProcessData, Process, VariableData, Variable };
